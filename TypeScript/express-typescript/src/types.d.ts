0fb5c7639620f296ba1adda110643aba904900d1
Franco Balich <franco.balich@gmail.com>
2022-08-01 20:51:05 -0300

    Add enums

--- TypeScript/express-typescript/src/types.d.ts
+++ TypeScript/express-typescript/src/types.d.ts
@@ -1,5 +1,20 @@
-export type Weather= 'sunny'|'rainy'|'cloudy'|'windy'|'stormy'
-export type Visibility = 'great'| 'good' |'ok'|'poor'
+// export type Weather= 'sunny'|'rainy'|'cloudy'|'windy'|'stormy'
+// export type Visibility = 'great'|'good' |'ok'|'poor'
+
+export enum Weather{
+  Sunny='sunny',
+  Rainy='rainy',
+  Cloudy='cloudy',
+  Windy='windy',
+  Stromy='stromy'
+}
+
+export enum Visibility{
+  Great='great',
+  Good='good',
+  Ok='ok',
+  Poor='poor'
+}
 
 export interface DiaryEntry{
   id: number
@@ -8,3 +23,9 @@ export interface DiaryEntry{
   visibility: Visibility
   comment: string
 }
+
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id'|'date'|'weather'|'visibility'>
+
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
