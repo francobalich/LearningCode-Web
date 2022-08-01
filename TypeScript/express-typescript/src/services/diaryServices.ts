d22e4fbc9ad0aee87b37e1501762a82f62ebe938
Franco Balich <franco.balich@gmail.com>
2022-08-01 20:51:28 -0300

    Add post

--- TypeScript/express-typescript/src/services/diaryServices.ts
+++ TypeScript/express-typescript/src/services/diaryServices.ts
@@ -1,8 +1,35 @@
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry, NewDiaryEntry } from '../types'
 import diaryData from './diaries.json'
 
 const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
-export const getEntries = () => diaries
+export const getEntries = (): DiaryEntry[] => diaries
 
-export const addEntry = () => null
+export const findById = (id: number): NonSensitiveInfoDiaryEntry|undefined => {
+  const entry = diaries.find(d => d.id === id)
+  if (entry != null) {
+    const { comment, ...restOfDiary } = entry
+    return restOfDiary
+  }
+  return undefined
+}
+
+export const getEntriesWitchoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return {
+      id,
+      date,
+      weather,
+      visibility
+    }
+  })
+}
+
+export const addDiary = (newDiaryEntry: NewDiaryEntry): DiaryEntry => {
+  const newDiary = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...newDiaryEntry
+  }
+  diaries.push(newDiary)
+  return newDiary
+}
