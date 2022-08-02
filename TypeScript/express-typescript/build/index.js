d6e729c3a27491f22e67a649d481a73d7cbd71c8
Franco Balich <franco.balich@gmail.com>
2022-08-01 21:02:49 -0300

    Build code

--- TypeScript/express-typescript/build/index.js
+++ TypeScript/express-typescript/build/index.js
@@ -1,16 +1,18 @@
-'use strict'
-const __importDefault = (this && this.__importDefault) || function (mod) {
-  return (mod && mod.__esModule) ? mod : { default: mod }
-}
-Object.defineProperty(exports, '__esModule', { value: true })
-const express = __importDefault(require('express'))
-const app = (0, express.default)()
-app.use(express.default.json()) // middleware que transffroma la req.body en un json
-const PORT = 3000
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("./routes/diaries"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json()); // middleware que transffroma la req.body en un json
+const PORT = 3000;
 app.get('/ping', (_req, res) => {
-  console.log('Alguien hizo un ping')
-  res.send('pong')
-})
+    console.log('Alguien hizo un ping ' + new Date().toLocaleDateString());
+    res.send('pong');
+});
+app.use('/api/diaries', diaries_1.default);
 app.listen(PORT, () => {
-  console.log(`El servidor esta corriendo en el puerto: ${PORT}`)
-})
+    console.log(`El servidor esta corriendo en el puerto: ${PORT}`);
+});
