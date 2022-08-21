754169b5d1e83573df1f7a2b04c1792c6aabc43a
Franco Balich <franco.balich@gmail.com>
2022-08-21 17:07:56 -0300

    Start proyect

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import freeCodeCampLogo from "./imgs/logo.png"
 
 function App() {
   return (
     <div className="App">
-
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt='Logo de freeCodeCamp'
+        />
+      </div>
     </div>
   );
 }
