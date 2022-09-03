0186fd031e6b80b53da3b60ab0e9c9b701dcc176
Franco Balich <franco.balich@gmail.com>
2022-09-03 15:19:25 -0300

    Add info in url

--- francochollos/src/App.jsx
+++ francochollos/src/App.jsx
@@ -1,17 +1,54 @@
 import './App.css'
-import { Route,Routes} from 'react-router-dom'
+import { Route, Routes, Link, useParams } from 'react-router-dom'
 
 const Home = () => <h1>Home</h1>
 
-const SearchPage = () => <h1>Search Page</h1>
-
+const SearchPage = () => {
+  const tacos = [
+    'cochinita',
+    'chili',
+    'carnita',
+    'quesadilla'
+  ]
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <ul>
+        {tacos.map(taco => (
+          <li key={taco}>
+            <Link to={`/tacos/${taco}`}>{taco}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+const Tacos = () => {
+  const { taco } = useParams()
+  return (
+    <div>
+      <h1>Tacos</h1>
+      {taco}
+    </div>
+  )
+}
 function App () {
   return (
     <div className='App'>
-      francochollo⚡
+      <header>
+        <h1>francochollo⚡</h1>
+        <nav>
+          <ul>
+            <li><Link to='/search-page'>Search Page</Link></li>
+            <li><Link to='/'>Home</Link></li>
+          </ul>
+        </nav>
+      </header>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/search-page' element={<SearchPage />} />
+        <Route path='/tacos/:taco' element={<Tacos />} />
 
       </Routes>
     </div>
