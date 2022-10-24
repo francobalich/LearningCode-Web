e8b30d33dee47c5902ad13b2e76173e9f3547919
Franco Balich <franco.balich@gmail.com>
2022-10-24 17:59:54 -0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import express from 'express'
-import {pool} from './db.js'
-
+import employeesRoutes from './routes/employees.routes.js'
+import indexRoutes from './routes/index.routes.js'
 const app = express()
 
-app.get('/ping',async (req,res)=> {
-    const [result]=await pool.query('SELECT "Pong" AS result')
-    res.json(result)
-})
-
-app.get('/employees',(req,res)=> res.send('obtenido empleados'))
-
-app.post('/employees',(req,res)=> res.send('creando empleados'))
-
-app.put('/employees',(req,res)=> res.send('actualizando empleados'))
-
-app.delete('/employees',(req,res)=> res.send('eliminando empleados'))
+app.use(indexRoutes)
+app.use(employeesRoutes)
 
 app.listen(3000)
 console.log('Server running on port 3000')
