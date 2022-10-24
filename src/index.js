3234d6ad0ec766b38f8d99192ae0787c799db45d
Franco Balich <franco.balich@gmail.com>
2022-10-24 17:49:00 -0300

    Add ping db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import {pool} from './db.js'
 
 const app = express()
+
+app.get('/ping',async (req,res)=> {
+    const [result]=await pool.query('SELECT "Pong" AS result')
+    res.json(result)
+})
 
 app.get('/employees',(req,res)=> res.send('obtenido empleados'))
 
