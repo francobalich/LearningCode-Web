7b2bbb69c593e14f878ab559082ee34d88046935
Franco Balich <franco.balich@gmail.com>
2022-10-24 17:39:49 -0300

    Add db script

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.get('/employees',(req,res)=> res.send('obtenido empleados'))
+
+app.post('/employees',(req,res)=> res.send('creando empleados'))
+
+app.put('/employees',(req,res)=> res.send('actualizando empleados'))
+
+app.delete('/employees',(req,res)=> res.send('eliminando empleados'))
+
 app.listen(3000)
+console.log('Server running on port 3000')
