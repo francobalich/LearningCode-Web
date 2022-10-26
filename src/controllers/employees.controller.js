966e0cd854d9152de2c9eacdcbba7c3ce9e3bc94
Franco Balich <franco.balich@gmail.com>
2022-10-26 18:19:05 -0300

    Add getEmployee

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -4,7 +4,11 @@ export const getEmployees = async(req,res)=> {
     const [rows]=await pool.query('SELECT * from employee')
     res.json({rows})
 }
-
+export const getEmployee = async(req,res)=> {
+    const [rows]=await pool.query('SELECT * from employee WHERE id=?;',[req.params.id])
+    if(rows.length<=0)return res.status(404).json({message:'Employee not found'})
+    res.json({rows})
+}
 export const createEmployee = async (req,res)=> {
     const {name,salary}= req.body
     const [rows]=await pool.query('INSERT INTO employee (name,salary) VALUES (?,?)',[name,salary])
@@ -17,4 +21,6 @@ export const createEmployee = async (req,res)=> {
 
 export const updateEmployee =(req,res)=> res.send('actualizando empleados')
 
-export const deleteEmployee =(req,res)=> res.send('eliminando empleados')
+export const deleteEmployee =(req,res)=> {
+    
+}
