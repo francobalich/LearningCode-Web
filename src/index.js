021da354c9bb2d64860c16f1aeb99b11c028fa18
Franco Balich <franco.balich@gmail.com>
2022-10-26 18:54:06 -0300

    Add 404 json

--- src/index.js
+++ src/index.js
@@ -9,5 +9,11 @@ app.use(express.json())
 app.use(indexRoutes)
 app.use('/api',employeesRoutes)
 
+app.use((req,res,next)=>{
+    res.status(404).json({
+        message:'Endpoint not found'
+    })
+})
+
 app.listen(3000)
 console.log('Server running on port 3000')
