309800ebb2dc5be025879070f356bbabbc0fd204
Franco Balich <franco.balich@gmail.com>
2022-11-19 22:59:24 -0300

    Add routes

--- react-router-protected/src/App.jsx
+++ react-router-protected/src/App.jsx
@@ -1,17 +1,34 @@
 import { useState } from 'react'
-import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import {BrowserRouter, Routes,Route,Link} from 'react-router-dom'
 import './App.css'
-import {Landing}  from './pages/index'
+import {Admin, Analytics, Dashboard, Home, Landing}  from './pages/index'
+
 function App() {
 
   return (
     <BrowserRouter>
+    <Navegation />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>}/>
+        <Route index element={<Landing />}/>
         <Route path="/landing" element={<Landing />}/>
+        <Route path="/home" element={<Home />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="/analytics" element={<Analytics />}/>
+        <Route path="/admin" element={<Admin />}/>
       </Routes>
     </BrowserRouter>
   )
+}
+function Navegation(){
+  return <nav>
+    <ul>
+      <li><Link to="/landing">Landing</Link></li>
+      <li><Link to="/home">Home</Link></li>
+      <li><Link to="/dashboard">Dashboard</Link></li>
+      <li><Link to="/analytics">Analytics</Link></li>
+      <li><Link to="/admin">Admin</Link></li>
+    </ul>
+  </nav>
 }
 
 export default App
